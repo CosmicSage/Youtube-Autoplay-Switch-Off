964bfaccd01bf73ea13906e7c788592b86ae98c1
CosmicSage <prabhakarmishra9@yahoo.com>
2019-12-08 22:29:10 +0530

    Changes

--- background.js
+++ background.js
@@ -2,13 +2,20 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log('The color is green.');
     });
-    chrome.tabs.executeScript({
-      file : "contentScript.js"
-    })
+
+    chrome.runtime.onMessage.addListener(
+    function(message, callback) {
+      if (message == "runContentScript"){
+        chrome.tabs.executeScript({
+          file: 'contentScript.js'
+        });
+      }
+   });
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'youtube.com/watch'},
+          pageUrl: {hostEquals: 'www.youtube.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,4 @@
+console.log("dfasgtfwed");
 if (document.getElementById("toggle").getAttribute("aria-pressed") === "true") {
   document.getElementById("toggle").click();
   console.log("Im called");
