29bca24a5a6e71dc1bc1f21d56812c5bef68f76f
CosmicSage <prabhakarmishra9@yahoo.com>
2019-12-08 20:16:56 +0530

    popup fix

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'youtube.com'},
+          pageUrl: {hostEquals: 'www.youtube.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
