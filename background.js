4762f77b0886b6b5fb2b63f1220dac01369ebbeb
CosmicSage <prabhakarmishra9@yahoo.com>
2019-12-08 21:41:45 +0530

    New

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'www.youtube.com'},
+          pageUrl: {hostEquals: 'youtube.com/watch'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
